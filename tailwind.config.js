5cda726dcd781eebb5722634a1fa0e7b07f0781e
elahevakili <147205901+elahevakili@users.noreply.github.com>
2025-09-29 13:44:39 +0330

    fixing root

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,19 @@
 import defaultTheme from 'tailwindcss/defaultTheme'
 
 export default {
-content: [
-  "./index.html",
-  "./contact.html",  
-  "./login.html",
-  "./register.html",        // ← این رو اضافه کن اگه فایل خارج از src هست
-  "./src/**/*.{js,ts,jsx,tsx}",
-  "./css/**/*.css"
-          // ← همه فایل‌های HTML در پروژه
-],
-theme: {
+  content: [
+    "./index.html",
+    "./contact.html",  
+    "./login.html",
+    "./register.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./css/**/*.css"
+  ],
+  theme: {
     extend: {
       fontFamily: {
-        iran: ['IranSans', ...defaultTheme.fontFamily.sans],
+      vazir: ['Vazir', 'sans-serif'],
       }
-     
     },
   },
   plugins: [],
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
+  import { defineConfig } from 'vite'
 
-export default defineConfig({
-  base: '/saftey-shop/', // حتما این رو درست وارد کن: اسم ریپازیتوری گیت‌هاب‌ت
-    })
+  export default defineConfig({
+    base: '/', // حتما این رو درست وارد کن: اسم ریپازیتوری گیت‌هاب‌ت
+      })
