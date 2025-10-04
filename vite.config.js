bee3ecf44717a703fcabf9d839ff2c262c2ae88f
elahevakili <147205901+elahevakili@users.noreply.github.com>
2025-10-04 10:45:47 +0330

    chore: تنظیم vite برای multi-page build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,25 @@
-  import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
-  export default defineConfig({
-    base:'./',
-      })
+export default defineConfig({
+  base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        cart: resolve(__dirname, 'public/cart.html'),
+        login: resolve(__dirname, 'public/login.html'),
+        register: resolve(__dirname, 'public/register.html'),
+        productDetail: resolve(__dirname, 'public/product-detail.html'),
+        productCategory: resolve(__dirname, 'public/product-category.html'),
+        adminOrders: resolve(__dirname, 'public/adminOrders.html'),
+        contact: resolve(__dirname, 'public/contact.html'),
+        success: resolve(__dirname, 'public/success.html'),
+        fail: resolve(__dirname, 'public/fail.html'),
+        forgetPassword: resolve(__dirname, 'public/forget-password.html'),
+        firefightingEquipment: resolve(__dirname, 'public/firefighting-equipment.html'),
+        safteyEquipment: resolve(__dirname, 'public/saftey-equipment.html'),
+      },
+    },
+  },
+})
