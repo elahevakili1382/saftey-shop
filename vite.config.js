7a744c155916985b1edcb873aac1d4abced88fe2
elahevakili <147205901+elahevakili@users.noreply.github.com>
2025-10-06 12:24:27 +0330

    convert it to root

--- vite.config.js
+++ vite.config.js
@@ -2,23 +2,23 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-  base: './',
+  base: './', // مسیر نسبی برای کارکرد درست در Vercel
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        cart: resolve(__dirname, 'public/cart.html'),
-        login: resolve(__dirname, 'public/login.html'),
-        register: resolve(__dirname, 'public/register.html'),
-        productDetail: resolve(__dirname, 'public/product-detail.html'),
-        productCategory: resolve(__dirname, 'public/product-category.html'),
-        adminOrders: resolve(__dirname, 'public/adminOrders.html'),
-        contact: resolve(__dirname, 'public/contact.html'),
-        success: resolve(__dirname, 'public/success.html'),
-        fail: resolve(__dirname, 'public/fail.html'),
-        forgetPassword: resolve(__dirname, 'public/forget-password.html'),
-        firefightingEquipment: resolve(__dirname, 'public/firefighting-equipment.html'),
-        safteyEquipment: resolve(__dirname, 'public/saftey-equipment.html'),
+        cart: resolve(__dirname, 'cart.html'),
+        login: resolve(__dirname, 'login.html'),
+        register: resolve(__dirname, 'register.html'),
+        productDetail: resolve(__dirname, 'product-detail.html'),
+        productCategory: resolve(__dirname, 'product-category.html'),
+        adminOrders: resolve(__dirname, 'adminOrders.html'),
+        contact: resolve(__dirname, 'contact.html'),
+        success: resolve(__dirname, 'success.html'),
+        fail: resolve(__dirname, 'fail.html'),
+        forgetPassword: resolve(__dirname, 'forget-password.html'),
+        firefightingEquipment: resolve(__dirname, 'firefighting-equipment.html'),
+        safteyEquipment: resolve(__dirname, 'saftey-equipment.html'),
       },
     },
   },
